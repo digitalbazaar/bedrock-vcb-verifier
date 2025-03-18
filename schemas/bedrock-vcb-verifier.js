64259e621d7b5b9c98fe13678843f9a1c12b0c3a
James Easter <jeaster@digitalbazaar.com>
2025-03-17 21:48:55 -0600

    Accept type property.

--- schemas/bedrock-vcb-verifier.js
+++ schemas/bedrock-vcb-verifier.js
@@ -4,16 +4,34 @@
 export const verifyBody = {
   title: 'Verify VCB Request',
   type: 'object',
-  additionalProperties: false,
+  additionalProperties: true,
   required: ['text'],
   properties: {
-    // FIXME: pass media type and/or format/type in another property or
     // allow use of conneg w/non-JSON payload
     text: {
       title: 'Text encoding of a VCB',
       type: 'string',
-      // must start with `VC1-` header
-      pattern: '^VC1-'
-    }
-  }
+    },
+    type: {
+      title: 'Type of scanned barcode',
+      type: 'string',
+      enum: [
+        // See https://wicg.github.io/shape-detection-api/#barcodeformat-section
+        'aztec',
+        'code_128',
+        'code_39',
+        'code_93',
+        'codabar',
+        'data_matrix',
+        'ean_13',
+        'ean_8',
+        'itf',
+        'pdf417',
+        'qr_code',
+        'unknown',
+        'upc_a',
+        'upc_e'
+      ]
+    },
+  },
 };
