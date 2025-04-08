a6a152b05bf300bdfdece49e6a3217494e8295f3
Dave Longley <dlongley@digitalbazaar.com>
2025-04-08 12:39:05 -0400

    Add support for barcode => `EnvelopedVerifiableCredential`.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -8,14 +8,40 @@ import {httpsAgent} from '@bedrock/https-agent';
 import {mockData} from './mock.data.js';
 
 describe('http API', () => {
-  const target = '/features/verify-vcb';
   describe('verify a VCB', () => {
+    const target = '/features/verify-vcb';
     let url;
     before(async () => {
       url = `${bedrock.config.server.baseUri}${target}`;
     });
 
-    it('succeeds when sending valid VCB text', async () => {
+    it('verifies VCB text transformed to an enveloped VC', async () => {
+      let err;
+      let result;
+      try {
+        const response = await httpClient.post(url, {
+          agent: httpsAgent,
+          json: {text: mockData.vcbText}
+        });
+        result = response.data;
+      } catch(e) {
+        err = e;
+      }
+      assertNoError(err);
+      should.exist(result);
+      result.should.include.keys(['credential', 'verified', 'expired']);
+      result.verified.should.equal(true);
+    });
+  });
+
+  describe('(legacy) verify a VCB', () => {
+    const target = '/features/legacy-verify-vcb';
+    let url;
+    before(async () => {
+      url = `${bedrock.config.server.baseUri}${target}`;
+    });
+
+    it('verifies VCB text locally converted from CBOR-LD', async () => {
       let err;
       let result;
       try {
